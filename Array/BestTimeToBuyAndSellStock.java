0c881b1a89b563caf034c9aea2db246bfbcef32b
Manavesh Narendra <manavesh.narendra@gmail.com>
2025-12-07 15:03:56 +0530

    Create BestTimeToBuyAndSellStock.java

---
+++ Array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,14 @@
+class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int currProfit = 0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            int diff = prices[i] - prices[i-1];
+            currProfit = Math.max(0, currProfit + diff);  // extend or reset
+            maxProfit = Math.max(maxProfit, currProfit);
+        }
+        
+        return maxProfit;
+    }
+}
